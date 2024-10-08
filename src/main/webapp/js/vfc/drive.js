d7f820fbc4d7ed0edd984ae52a0c1202e8f87f8d
이수정 <crystal95@neighbor21.co.kr>
2024-10-08 14:43:00 +0900

    [2024-10-08 이수정] 운전면허확인-주석제거, 확인알럿 제거

--- src/main/webapp/js/vfc/drive.js
+++ src/main/webapp/js/vfc/drive.js
@@ -786,15 +786,6 @@ var similarityImage = false; // 유사도 검증 이미지유무 전역변수
 			});
 
 			$('.license_btn').click(function() {
-//				if(similarityChk){
-//					$("#similarityChk").prop('checked', false);
-//					$(".similarityChkBox").css("display", "none");
-//					$("#similarity_tb_top").css("display", "none");
-//					$drive.event.similarityChkFn();
-//					alert("모바일 면허증 선택 시 유사도 검증이 불가합니다.")
-//				}
-//				
-//				console.log(similarityChk)
 				$('#all_chk').prop("checked", false);
 				$('input[type=checkbox][name=agreeInfo]').prop('checked', false);
 				$("#qrCodeArea").css('display', 'none');
@@ -1070,30 +1061,18 @@ var similarityImage = false; // 유사도 검증 이미지유무 전역변수
 			$('#similarityChk').on('change', function() {
 				if ($('#similarityChk').prop('checked')) {
 					similarityChk = true
-					//console.log(similarityChk)
 					// 유사도 검증 안내 팝업
 					$(".similarity_pop").css("display", "flex");
 				} else {
 					similarityChk = false
-					//console.log(similarityChk)
 				}
 				
-			    //$drive.event.similarityChkFn(); // 호출할 함수
 			});
 			
 			$(".similarity_pop .close").on("click",function(){
 			    $(".similarity_pop").css("display", "none");
 			});
 		
-//			$("#qrCancle").on("click",function(){
-//				if(userType!="PC"){
-//					$(".similarityChkBox").css("display", "");
-//					$("#similarity_tb_top").css("display", "");
-//					$('#similarityChk').prop('checked', false)
-//				}
-//			});
-
-
 		},
 
         // 2024.07.31 jeonghyewon code add
@@ -1279,7 +1258,6 @@ var similarityImage = false; // 유사도 검증 이미지유무 전역변수
         
 		// 웹으로 결과 전달(2)  // 앱테스트중
 		alcheraCheckResult: function(json) {
-			alert("alcheraCheckResult 도착");
 			var data = JSON.parse(json);
 			if(data.similarityConfidence != null && data.livenessConfidence != null) {
 				$drive.event.verifyLicense(data);
@@ -1544,49 +1522,6 @@ var similarityImage = false; // 유사도 검증 이미지유무 전역변수
 			vrfcMthd = 1;
 		},
 		
-//		similarityChkFn: function() {
-//			if ($('#similarityChk').prop('checked')) {
-//				similarityChk = true
-//				console.log(similarityChk)
-//				// 유사도 검증 안내 팝업
-//				$(".similarity_pop").css("display", "flex");
-//				
-//				// 값 초기화
-////				$("#num01").data("kendoDropDownList").select(0);
-////				$('#user_tel').val('');
-////				$('#license_num').val('');
-////				$('#name').val('');
-////				$('#num02').val('');
-////				$('#num03').val('');
-////				$('#num04').val('');
-////				$("input[type=radio][name=category01]").prop('checked', false);
-////	//			$("#name").attr('disabled', false);
-////				$("#num01").data("kendoDropDownList").readonly(false);
-////				$("#num02").attr('disabled', false);
-////				$("#num03").attr('disabled', false);
-////				$("#num04").attr('disabled', false);
-////				$('input[type=radio]').attr("disabled", false);
-////				vrfcMthd = 1;
-//				
-//				// 리드온리 처리
-//				$(".license_bg input").attr('readonly', true);
-//				$("#num01").data("kendoDropDownList").readonly();
-//				$("#num01").closest("span").css("background-color", "#f5f5f5"); 
-//				$('.license_bg input[type="radio"]').attr('disabled', true);
-//				$('.license_bg input').css('background-color', '#f5f5f5');
-//
-//
-//			} else {
-//				similarityChk = false
-//				console.log(similarityChk)
-//				// 리드온리 해제
-//				$('.license_bg input').removeAttr('readonly');
-//				$('.license_bg input[type="radio"]').removeAttr('disabled');
-//				$('.license_bg input').css('background-color', '');
-//				$("#num01").data("kendoDropDownList").readonly(false);
-//				$("#num01").closest("span").css("background-color", "");
-//			}
-//		}
 	};
 
 }(window, document, jQuery));
