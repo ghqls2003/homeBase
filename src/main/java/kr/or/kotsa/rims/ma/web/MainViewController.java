aca0573e7532276e4ec0be87a68d44f38ce99100
Ryong <kryong@neighbor21.co.kr>
2024-10-14 10:41:07 +0900

    메인 user헤더 조건 수정

--- src/main/java/kr/or/kotsa/rims/ma/web/MainViewController.java
+++ src/main/java/kr/or/kotsa/rims/ma/web/MainViewController.java
@@ -167,17 +167,16 @@ public class MainViewController extends CmmnAbstractServiceImpl{
 	        return IS_PC;
 	    }
 	}
-	
+
 	public static boolean isAccMthd(HttpServletRequest req) {
 		String userAgent = req.getHeader("User-Agent").toUpperCase();
+		System.out.println("뭐야"+userAgent);
 		if(userAgent.contains("INRIMSAPP")) {
-			return IS_MW;
-		} else if(userAgent.contains("IPHONE") && !userAgent.contains("SAFARI")) {
 			return IS_MW;
 		} else {
 			return true;
 		}
-	}
+	}	
 	
 	/**
 	 * 회원가입 화면
